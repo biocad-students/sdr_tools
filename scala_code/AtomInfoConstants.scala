4142a638413a2c0c4320677128de1828e8ac05c2
Tatiana Malygina <merlettaia@gmail.com>
2015-08-04 20:40:33 +0300

    adding constants and lattice obj - wip

---
+++ scala_code/AtomInfoConstants.scala
@@ -0,0 +1,6 @@
+package ru.biocad.ig.common.structures.atom
+
+//TODO: this file should contain some chemistry constants - table of atom weights, van-der-waals radii, etc.
+//also loads constants from files (energies, etc.)
+object AtomInfoConstants {
+}
