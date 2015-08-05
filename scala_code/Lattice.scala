9c73d306492ae1a8d7821934a3c666e1502c95d3
Tatiana Malygina <merlettaia@gmail.com>
2015-08-05 03:03:03 +0300

    wip - init energy definition for H-bond, some energy constants, partial implementation of other energy components

--- scala_code/Lattice.scala
+++ scala_code/Lattice.scala
@@ -5,7 +5,13 @@ Later should refactor to class with file parameters loading, thus making creatio
 */
 object LatticeConstants {
   val MESH_SIZE = 1.7
-  val CA_ANGLE_CONSTRAINTS = {"min": 78.5, "max": 143.1}
+
+  object Borders extends Enumeration() {
+    val Min, Max = Value
+  }
+  import Borders._
+
+  val CA_ANGLE_CONSTRAINTS = Map(Min -> 78.5, Max -> 143.1)
   val CA_MIN_DISTANCE = 0.0 // unspecified
 
   //H-bond energies
@@ -13,7 +19,13 @@ object LatticeConstants {
   val E_HH = 0.75
   val H_bond_R_min = 4.8
   val H_bond_R_max = 7.0
+  val H_bond_a_max = 17.3
   def H_bond_distance_condition(r: Double): Boolean = r >= H_bond_R_min&& r <= H_bond_R_max
+  //rotamers
+  //val ROTAMERS = Map()
+  //TODO: implement as a map number of rotamers, maybe with ProteinAlphabet class from ig-toolkit
+  //contact
+  val CONTACT_CUTOFF = 4.2
 }
 
 object Lattice {
@@ -21,31 +33,52 @@ object Lattice {
   //this returns true if they can, false otherwise - quite simple
   def can_form_H_bond(aminoacids: Seq[SimplifiedAminoAcid], i : Int, j : Int) : Boolean = {
     val r_ij = aminoacids(j).ca - aminoacids(i).ca
-    i - j >= 3 && LatticeConstants.H_bond_distance_condition(r_ij.length) 
+    val b_i_b_i_1 = aminoacids(i - 1).ca - aminoacids(i).ca //TODO: check if i == 0
+    val b_j_b_j_1 = aminoacids(j - 1).ca - aminoacids(j).ca
+    i - j >= 3 && LatticeConstants.H_bond_distance_condition(r_ij.length) &&
+      (b_i_b_i_1*r_ij).abs <= LatticeConstants.H_bond_a_max &&
+      (b_j_b_j_1*r_ij).abs <= LatticeConstants.H_bond_a_max
     //LatticeConstants
   }
   /** energy methods*/
   def get_E_CA_trace(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
-
+    ???
   }
+
   def get_E_H_bond(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
-    can_form_H_bond(aminoacids, i, j)
-
+    var E = 0.0
+    (1 to aminoacids.size).foreach( i => {
+      (i to aminoacids.size).foreach(j => {
+        if (can_form_H_bond(aminoacids, i, j)) {
+          E += LatticeConstants.E_H
+          //FIX: consider cooperativity
+          //FIX: consider condition - each aminoacid can form no more than 2 H-bonds (proline - no more than 1 bond)
+          //FIX: maximize
+        }
+      })
+    }
+    )
+    E
   }
+
   def get_E_rot(aminoacids : Seq[SimplifiedAminoAcid]): Double = {
-
+    ???
   }
+
   def get_E_SG_local(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
-
+    ???
   }
+
   def get_E_one(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
-
+    ???
   }
+
   def get_E_pair(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
-
+    ???
   }
-  def get_E_tem(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
 
+  def get_E_tem(aminoacids : Seq[SimplifiedAminoAcid]) : Double = {
+    ???
   }
 
   //TODO: we have pair of chains, that means we somehow should utilize that when we compute total energy
--- scala_code/MCTools.scala
+++ scala_code/MCTools.scala
@@ -11,25 +11,35 @@ import ru.biocad.ig.common.io.pdb.{PDBStructure, PDBAtomInfo, PDBAminoAcidCollec
 
 }*/
 
-class Rotamer(val atoms : Seq[PDBAtomInfo], val ca : PDBAtomInfo) {
-  val center = atoms.foldLeft(Vector3d(0,0,0)) ({case (result, atom) => {
+class Rotamer(val atoms : Seq[PDBAtomInfo], val ca : GeometryVector) {
+  /*
+  val center : GeometryVector = atoms.foldLeft(Vector3d(0, 0, 0)) ({case (result, atom) => {
     //todo: add weight to atom center determination
     result + Vector3d(atom.x, atom.y, atom.z)
-  }})/atoms.size
+  }}) ///atoms.size
   val center_relative = center //todo: add relativity
-
+*/
 }
 
 class SimplifiedAminoAcid(val atoms : Seq[PDBAtomInfo]) {
-  val ca = atoms.find{_.atom == "CA"}
+  val ca : GeometryVector = atoms.find{_.atom == "CA"} match{
+    case Some(value) => Vector3d(value.x, value.y, value.z)
+    case None => Vector3d(0.0, 0.0, 0.0)
+    }
+  //FIX: ..
   /** there should be rotamer center+radius.
   If rotamer center is changed and ca moved,  we should recompute all atoms when moving back to full-atom model.
   If there were no changes, we can probably use original coordinates of atoms.
   */
   //rotamer has off-lattice coordinates vs. Ca's are projected onto lattice
-  var rotamer : Rotamer = new Rotamer(atoms.filterNot{_.atom in Seq("N", "H")})//todo: check this
-  def isInContactWith(aa : SimplifiedAminoAcid) : Boolean = {
-    return false
+  var rotamer : Rotamer = new Rotamer(atoms.filterNot{s=>Seq("N", "H").contains(s.atom)}, ca)//todo: check this
+  def isInContactWith(aa : SimplifiedAminoAcid, distance_cutoff : Double = 4.2) : Boolean = {
+    atoms.forall({case atom => {
+      val atom_vector = Vector3d(atom.x, atom.y, atom.z)
+      val len = aa.atoms.map({case atom2 => (atom_vector - Vector3d(atom2.x, atom2.y, atom2.z)).length}).min
+      len > distance_cutoff
+    }})
+    //return false
     //TODO: should implement
   }
 }
