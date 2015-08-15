39f73704cb4363f6f70f4f69b2ee40e944539b49
Tatiana Malygina <merlettaia@gmail.com>
2015-08-15 09:27:14 +0300

    added basic test for aminoacid simplification

--- scala_code/MCTools.scala
+++ scala_code/MCTools.scala
@@ -12,17 +12,16 @@ import ru.biocad.ig.common.io.pdb.{PDBStructure, PDBAtomInfo, PDBAminoAcidCollec
 }*/
 
 class Rotamer(val atoms : Seq[PDBAtomInfo], val ca : GeometryVector) {
-  /*
-  val center : GeometryVector = atoms.foldLeft(Vector3d(0, 0, 0)) ({case (result, atom) => {
-    //todo: add weight to atom center determination
-    result + Vector3d(atom.x, atom.y, atom.z)
-  }}) ///atoms.size
-  val center_relative = center //todo: add relativity
-*/
+  val sidechainRelativeVectors = atoms.map({atom=> Vector3d(atom.x, atom.y, atom.z) - ca})
+  val center : GeometryVector = if (sidechainRelativeVectors.size > 0)
+      sidechainRelativeVectors.reduceLeft (_ + _) / sidechainRelativeVectors.size 
+    else Vector3d(0,0,0)
+
+  override def toString = sidechainRelativeVectors.mkString("[[", ",\n", "]]") + "\n, center = " + center.toString
 }
 
 class SimplifiedAminoAcid(val atoms : Seq[PDBAtomInfo]) {
-  val ca : GeometryVector = atoms.find{_.atom == "CA"} match{
+  val ca : GeometryVector = atoms.find{_.atom.trim == "CA"} match{
     case Some(value) => Vector3d(value.x, value.y, value.z)
     case None => Vector3d(0.0, 0.0, 0.0)
     }
@@ -32,7 +31,7 @@ class SimplifiedAminoAcid(val atoms : Seq[PDBAtomInfo]) {
   If there were no changes, we can probably use original coordinates of atoms.
   */
   //rotamer has off-lattice coordinates vs. Ca's are projected onto lattice
-  var rotamer : Rotamer = new Rotamer(atoms.filterNot{s=>Seq("N", "H").contains(s.atom)}, ca)//todo: check this
+  var rotamer : Rotamer = new Rotamer(atoms.filterNot{s=>Seq("N", "H", "CA", "C", "O").contains(s.atom.trim)}, ca)//todo: check this
   def isInContactWith(aa : SimplifiedAminoAcid, distance_cutoff : Double = 4.2) : Boolean = {
     atoms.forall({case atom => {
       val atom_vector = Vector3d(atom.x, atom.y, atom.z)
@@ -42,4 +41,13 @@ class SimplifiedAminoAcid(val atoms : Seq[PDBAtomInfo]) {
     //return false
     //TODO: should implement
   }
+
+  override def toString = Seq(
+    "original atoms: ",
+    atoms.mkString("\n[ \n  ", ", \n  ", " ],"),
+    "\nca: ",
+    ca.toString + ",",
+    "\nrotamer: ",
+    rotamer.toString
+  ).mkString("SimplifiedAminoacid{\n", "\n ", "\n}")
 }
