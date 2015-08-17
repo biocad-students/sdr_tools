8408e646ec3666c7cf85d90930d4c9b68ef33ffe
Tatiana Malygina <merlettaia@gmail.com>
2015-08-17 19:44:07 +0300

    wip

--- julia_scripts/backbone_statistics.jl
+++ julia_scripts/backbone_statistics.jl
@@ -48,17 +48,66 @@ type PDBAtomInfo
   charge :: String
 end
 
-function parseAtomInfoFromString(str :: String)
-  #TODO: add parser and check correctness
+function parseAtomInfoFromString(line :: String)
+  #TODO: check correctness
+  #println(line)
+  result = PDBAtomInfo(
+      int(line[6:12]),
+      strip(line[13:16]),
+      line[17],
+      strip(line[17:20]),
+      line[22],
+      int(line[23:26]),
+      line[27],
+      float(strip(line[31:38])),
+      float(strip(line[39:46])),
+      float(strip(line[47:54])),
+      float(strip(line[55:60])),
+      float(strip(line[61:66])),
+      strip(line[76:78]),
+      strip(line[79:80]))
+  #println(result)
+  result
 end
 
+#immutable SlidingWindow
+#    iter
+#    width :: Int
+#end
+
+#function take(iter, n :: Int)
+#  state = start(iter)
+#  c = 1
+#  result = Array{AbstractArray, 1}()
+#  while !done(iter, state) && c < n
+#    (i, state) = next(iter, state)
+#    push!(result, i)
+#    c += 1
+#  end
+#  result
+#end
+
+#Base.start(S::SlidingWindow) = take(S.iter, S.width)
+#Base.next(S::SlidingWindow, state) = (take(S.iter, S.width), next(S.iter))
+#Base.done(S::SlidingWindow, s) = length(s) >= S.width;
+
+
+
 function readPDB(input_file_name :: String)
-  records = PDBAtomInfo[]
+  records = Dict{Char, Dict{Int, Array{PDBAtomInfo, 1}}}()
   input_file = open(input_file_name, "r")
   while !eof(input_file)
     s = rstrip(readline(input_file), ['\r','\n'])
     if s[1:4] == "ATOM"
-      push!(records, parseAtomInfoFromString(s))
+      atom = parseAtomInfoFromString(s)
+      if !(atom.chainID in keys(records))
+        records[atom.chainID] = Dict{Int, Array{PDBAtomInfo, 1}}()
+      end
+      if !(atom.resSeq in keys(records[atom.chainID]))
+        records[atom.chainID][atom.resSeq] = PDBAtomInfo[]
+      end
+      push!(records[atom.chainID][atom.resSeq], atom)
+      #push!(records, parseAtomInfoFromString(s))
     end
   end
   records
@@ -66,10 +115,18 @@ end
 
 function load_atom_info(pdb_file_name)
   atom_infos = readPDB(pdb_file_name)
-  
+  for chain in keys(atom_infos)
+    width = 4
+    ks = [k for k in keys(atom_infos[chain])]
+    for k in width : length(keys(atom_infos[chain]))
+      println(ks[k-width+1: k])
+    end
+  end
+  #println(atom_infos)
 end
 
 function main()
+  load_atom_info("2OSL.pdb")
     #parsed_args = parse_commandline()
 
     #input_file = parsed_args["fasta-file"]
