2130d6832d76f2fe6917c5d9d56348d2e0626cee
Tatiana Malygina <merlettaia@gmail.com>
2015-08-21 19:09:37 +0300

    added convertion from String to Int while getting data from json with int keys in Map

--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/AlascanConstants.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/AlascanConstants.scala
@@ -1,7 +1,7 @@
 package ru.biocad.ig.alascan.constants
 
 case class BackboneInfo(
-  val data : Map[String, Map[String, Map[String, Map[String, Map[String, Seq[Double]]]]]],
+  val data : Map[String, Map[Int, Map[Int, Map[Int, Map[String, Seq[Double]]]]]],
   val meshSize : Double) {
 
 }
--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/json/Protocol.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/json/Protocol.scala
@@ -5,11 +5,23 @@ import ru.biocad.ig.alascan.constants.{BackboneInfo}
 
 
 object AlascanConstantsJsonProtocol extends DefaultJsonProtocol {
-  implicit val alascanConstantsFormat : JsonFormat[BackboneInfo] = jsonFormat2(BackboneInfo)
 
-  //implicit object AlascanConstantsJsonFormat extends RootJsonFormat[BackboneInfo] {
-  //  def write(info: BackboneInfo) = info.toJson
+  implicit object AlascanConstantsJsonFormat extends RootJsonFormat[BackboneInfo] {
+    def write(info: BackboneInfo) = info.toJson
 
-//    def read(value: JsValue) =  new BackboneInfo(value)
-//}
+    def read(value: JsValue) = {
+      value.asJsObject.getFields("data", "meshSize") match {
+        case Seq(JsObject(data), JsNumber(meshSize)) => {
+          new BackboneInfo(data.mapValues(_.convertTo[Map[String, JsValue]].map({
+            case (k, v) => (k.toInt, v.convertTo[Map[String, JsValue]].map({
+              case (k, v) => (k.toInt, v.convertTo[Map[String, Map[String, Seq[Double]]]].map({
+            case (k,v) => (k.toInt, v)
+          }))}))})), meshSize.toDouble)
+          }
+        case _ => throw new DeserializationException("BackboneInfo expected")
+      }
+    }
+  }
+
 }
+import AlascanConstantsJsonProtocol._
