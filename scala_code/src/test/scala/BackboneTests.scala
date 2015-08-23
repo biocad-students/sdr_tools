2a03f1432cf09d842acb7373a56848df3180ee49
Tatiana Malygina <merlettaia@gmail.com>
2015-08-23 23:11:08 +0300

    added basic backbone loading test

---
+++ scala_code/src/test/scala/BackboneTests.scala
@@ -0,0 +1,17 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+import ru.biocad.ig.alascan.constants.BackboneInfo
+import spray.json._
+import scala.io.Source
+
+import ru.biocad.ig.alascan.constants.json.AlascanConstantsJsonProtocol
+import ru.biocad.ig.alascan.constants.json.AlascanConstantsJsonProtocol._
+
+class BackboneTests extends FlatSpec with Matchers {
+  it should "restore coordinates with given meshSize" in {
+    val backboneInfo = JsonParser(Source.fromURL(getClass.getResource("/backbone.json")).getLines().mkString("")).convertTo[BackboneInfo]
+    (backboneInfo.data("LEU")(20)(22)(-32)) should equal (
+      backboneInfo.restoreCoordinates("LEU",
+        20*backboneInfo.meshSize, 22*backboneInfo.meshSize, -32*backboneInfo.meshSize))
+  }
+}
