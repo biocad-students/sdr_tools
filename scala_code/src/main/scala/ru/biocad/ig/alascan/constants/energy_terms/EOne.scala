39822a7220a29fbe24ab8c3bac9187f595c9852f
Tatiana Malygina <merlettaia@gmail.com>
2015-09-02 02:25:20 +0300

    added eone converted 2 json file + basic class for loading it contents to it

---
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/energy_terms/EOne.scala
@@ -0,0 +1,12 @@
+package ru.biocad.ig.alascan.constants.energy_terms
+
+import spray.json._
+
+case class EOne(val nkb : Array[Int], val eone: Array[Double]) {
+}
+import DefaultJsonProtocol._
+object EOneJsonProtocol extends DefaultJsonProtocol {
+  implicit val colorFormat = jsonFormat2(EOne)
+}
+
+import EOneJsonProtocol._
