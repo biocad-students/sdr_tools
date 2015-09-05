18c4a74ec07e803e41155012c86b34a8f5b29959
Tatiana Malygina <merlettaia@gmail.com>
2015-09-06 01:18:42 +0300

    moving bins to the right json class - still adding ramachandran potentials

--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/energy_terms/E14.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/energy_terms/E14.scala
@@ -3,13 +3,41 @@ package ru.biocad.ig.alascan.constants.energy_terms
 import spray.json._
 import DefaultJsonProtocol._
 
+//FIX: naming according to original parameter names
+//NOTE: according to MCDP.info, there should be some preprocessing here.
+//currently I'm loading r14avg12.json and r14aa12.json separately, with no preprocessing
+/*this class contains average values from r14avg12.json*/
+case class E14avg(val aros : Array[Array[Double]],
+                  val e14 : Array[Double],
+                  val startIndex : Int,
+                  val endIndex : Int,
+                  val binRightBorders : Seq[Int]) {
 
-case class E14aa(val aa1: String, val aa2:String, val e214 : Array[Double], val n14 : Int) {
+  def getBinNumber(r14 : Double) : Int = {
+    val x = math.round(r14).toInt
+    binRightBorders.zipWithIndex.find({ c => x < c._1}) match {
+        case Some(v) => v._2
+        case _ => binRightBorders.size
+      }
+  }
+
+  def get(r14 : Double, r14_prev : Double) : Double = {
+    return aros(getBinNumber(r14))(getBinNumber(r14_prev))
+  }
 }
-object E14aaProtocol extends DefaultJsonProtocol {
-  implicit val E14aaFormat = jsonFormat4(E14aa)
+
+object E14avgJsonProtocol extends DefaultJsonProtocol {
+  implicit val E14avgJsonFormat = jsonFormat5(E14avg)
 }
-import E14aaProtocol._
+import E14avgJsonProtocol._
+
+/*this class contains values from r14aa12.json, it is hidden within E14 class*/
+case class E14aa(val aa1 : String, val aa2 : String, val e214 : Array[Double], val n14 : Int) {
+}
+object E14aaJsonProtocol extends DefaultJsonProtocol {
+  implicit val E14aaJsonFormat = jsonFormat4(E14aa)
+}
+import E14aaJsonProtocol._
 
 case class E14(
       val data : Map[String, Map[String, E14aa]],
@@ -23,14 +51,6 @@ case class E14(
     case _ => endIndex - startIndex
   }
 
-  def getBinNumber(r14 : Double) : Int = {
-    val x = math.round(r14).toInt
-    binRightBorders.zipWithIndex.find({ c => x < c._1}) match {
-        case Some(v) => v._2
-        case _ => binRightBorders.size
-      }
-  }
-
   /*method returns energy for current aminoacid pair and R14 distance for given aminoacid i*/
   def get(r14 : Double, aa1 : String, aa2 : String) : Double = {
     if (!data.contains(aa1))
@@ -38,11 +58,7 @@ case class E14(
     else if (!data(aa1).contains(aa2)) {
        return get(r14, aa2, aa1)
     }
-
-    val index = getValidIndex(r14)
-    println(data(aa1)(aa2).e214(index))
-      //println(getBinNumber(r14))
-    ???
+    return data(aa1)(aa2).e214(getValidIndex(r14))
   }
 }
 
