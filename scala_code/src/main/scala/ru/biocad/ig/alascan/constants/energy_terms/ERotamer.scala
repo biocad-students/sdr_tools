b70cda3a4f3cd5ab14c0b9d01f9bd296299905ad
Tatiana Malygina <merlettaia@gmail.com>
2015-10-19 08:04:16 +0300

    rotamer energies are computed

--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/energy_terms/ERotamer.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/energy_terms/ERotamer.scala
@@ -3,13 +3,30 @@ package ru.biocad.ig.alascan.constants.energy_terms
 import spray.json._
 import DefaultJsonProtocol._
 import ru.biocad.ig.common.structures.aminoacid.SimplifiedAminoAcid
+import ru.biocad.ig.common.structures.geometry.GeometryVector
 
 case class ERotamer(
-      val phi : Map[Int, Array[Double]],
-      val energies : Map[String, Map[Int, Array[Double]]]) {
+      val phi : Map[Int, Seq[Double]],
+      val energies : Map[String, Map[Int, Seq[Double]]]) {
 
-  def get(aa1 : SimplifiedAminoAcid, aa2 : SimplifiedAminoAcid, distance : Int) : Double = {
-    1.0
+  def get(current : SimplifiedAminoAcid, prev : SimplifiedAminoAcid, next : SimplifiedAminoAcid) : Double = {
+    val v1 : GeometryVector = prev.ca - current.ca
+    val v2 : GeometryVector = next.ca - current.ca
+    val distance : Int = (prev.ca - next.ca).lengthSquared.toInt
+    println(distance)
+    if (phi.contains(distance)) {
+      val angle = Math.round(Math.toDegrees(Math.acos((v1*v2) /(v1.length*v2.length))))
+      phi(distance).foreach(println)
+      println(angle)
+      val index = phi(distance).indexWhere({case x => Math.abs(x - angle) < 1.0}) // TODO: add epsilon or some sort of approx
+      if (index >= 0){
+        val values = energies.getOrElse(current.name, Map[Int, Seq[Double]]()).getOrElse(distance, Seq[Double]())
+        if (index < values.size)
+          return values(index)
+      }
+        //
+    }
+    0.0
   }
 }
 
@@ -23,8 +40,8 @@ object ERotamerJsonProtocol extends DefaultJsonProtocol {
       value.asJsObject.getFields("phi", "energies") match {
         case Seq(JsObject(phi), JsObject(energies)) => {
           new ERotamer(
-            phi.map({case (k, v) => (k.toInt, v.convertTo[Array[Double]])}),
-            energies.mapValues(_.convertTo[Map[String, Array[Double]]].map(
+            phi.map({case (k, v) => (k.toInt, v.convertTo[Seq[Double]])}),
+            energies.mapValues(_.convertTo[Map[String, Seq[Double]]].map(
               {case (k, v) => (k.toInt, v)}))
           )
         }
