87e7343f75fe06907eb3c01d571733a5c72b5448
Tatiana Malygina <merlettaia@gmail.com>
2015-10-23 10:13:36 +0300

    added sidechainInfo converter to PDB atom lines

--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/SidechainInfo.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/SidechainInfo.scala
@@ -13,10 +13,17 @@ case class SidechainInfo(
 
     override def getPDBAtomInfo(aminoacid : SimplifiedAminoAcid,
             x : GeometryVector, y : GeometryVector, z : GeometryVector) : Seq[PDBAtomInfo] = {
+        val a = representatives.sortWith({(a, b) =>
+          (aminoacid.rotamer.center - a.center).lengthSquared < (aminoacid.rotamer.center - b.center).lengthSquared})
+        //println(a.head.atoms)
+        a.head.atoms.map({case (k, v) =>
+        (k, AminoacidUtils.getGlobalCoordinates(Seq(x, y, z), v.toSeq) )}).map({
+          case (k, v) => aminoacid.getUpdatedAtomInfo(k, v)
+        }).toSeq
         //println("11111")
         /*data.map({
           case (k, v) => (k, AminoacidUtils.getGlobalCoordinates(Seq(x, y, z), v.toSeq))
         }).map({case (k, v) => aminoacid.getUpdatedAtomInfo(k, v) }).toSeq**/
-        ??? //TODO: should implement
+        //??? //TODO: should implement
     }
 }
