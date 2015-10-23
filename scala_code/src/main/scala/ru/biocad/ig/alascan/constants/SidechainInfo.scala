f423dc72df25a057cb9a09f7987dcd96a60ee799
Tatiana Malygina <merlettaia@gmail.com>
2015-10-23 15:54:27 +0300

    added helper methods for rotamer move

--- scala_code/src/main/scala/ru/biocad/ig/alascan/constants/SidechainInfo.scala
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/constants/SidechainInfo.scala
@@ -6,6 +6,8 @@ import ru.biocad.ig.common.structures.aminoacid.SimplifiedAminoAcid
 import ru.biocad.ig.common.structures.geometry.GeometryVector
 import ru.biocad.ig.common.algorithms.geometry.AminoacidUtils
 
+import java.util.Random
+
 case class SidechainInfo(
   val representatives : Seq[Rotamer],
   val amounts : Seq[Int],
@@ -25,5 +27,18 @@ case class SidechainInfo(
           case (k, v) => (k, AminoacidUtils.getGlobalCoordinates(Seq(x, y, z), v.toSeq))
         }).map({case (k, v) => aminoacid.getUpdatedAtomInfo(k, v) }).toSeq**/
         //??? //TODO: should implement
+    }
+
+    def setRotamerFromLibrary(aminoacidToModify : SimplifiedAminoAcid,
+      d1:Double, d2:Double, d3:Double) = {
+        val a = representatives.sortWith({a, b} => (aminoacidToModify.rotamer.center - a.center).lengthSquared <
+        (aminoacidToModify.rotamer.center - b.center).lengthSquared)
+        aminoacidToModify.rotamer = a.head
+      }
+    def changeRotamerToRandom(aminoacidToModify : SimplifiedAminoAcid,
+    d1:Double, d2:Double, d3:Double) = {
+      val a = representatives.sortWith({a, b} => (aminoacidToModify.rotamer.center - a.center).lengthSquared <
+      (aminoacidToModify.rotamer.center - b.center).lengthSquared)
+      aminoacidToModify.rotamer = Random.shuffle(a.tail).head
     }
 }
