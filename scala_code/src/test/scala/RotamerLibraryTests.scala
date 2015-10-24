eeed749ce4d5342e473dcceecad3d76a107fb128
Tatiana Malygina <merlettaia@gmail.com>
2015-10-24 09:53:28 +0300

    added couple of tests for rotamer library's methods

--- scala_code/src/test/scala/RotamerLibraryTests.scala
+++ scala_code/src/test/scala/RotamerLibraryTests.scala
@@ -7,12 +7,29 @@ import ru.biocad.ig.alascan.constants.json.SidechainLibraryJsonProtocol
 import ru.biocad.ig.alascan.constants.json.SidechainLibraryJsonProtocol._
 
 import ru.biocad.ig.alascan.constants.{AminoacidLibrary, SidechainInfo}
+import ru.biocad.ig.common.structures.aminoacid.SimplifiedAminoAcid
 
 class RotamerLibraryTests extends FlatSpec with Matchers {
   it should "restore coordinates with given meshSize" in {
     val rotamerInfo = JsonParser(Source.fromURL(getClass.getResource("/sidechains.json")).getLines().mkString("")).convertTo[AminoacidLibrary[SidechainInfo]]
     (rotamerInfo.data("LEU")(20)(22)(-32)) should equal (
-      rotamerInfo.restoreInfo("LEU",
+      rotamerInfo.restoreAminoAcidInfo("LEU",
         20*rotamerInfo.meshSize, 22*rotamerInfo.meshSize, -32*rotamerInfo.meshSize))
+  }
+  it should "correctly call setRotamerFromLibrary for empty SidechainInfo" in {
+    val s = SidechainInfo(Seq(), Seq(), 0)
+    val aa = new SimplifiedAminoAcid(Seq())
+    noException should be thrownBy s.setRotamerFromLibrary(aa)
+    val result = s.setRotamerFromLibrary(aa)
+    result should equal(aa)
+    (result.rotamer) should equal (aa.rotamer)
+  }
+  it should "correctly call changeRotamerToRandom for empty SidechainInfo" in {
+    val s = SidechainInfo(Seq(), Seq(), 0)
+    val aa = new SimplifiedAminoAcid(Seq())
+    noException should be thrownBy s.changeRotamerToRandom(aa)
+    val result = s.changeRotamerToRandom(aa)
+    result should equal(aa)
+    (result.rotamer) should equal (aa.rotamer)
   }
 }
