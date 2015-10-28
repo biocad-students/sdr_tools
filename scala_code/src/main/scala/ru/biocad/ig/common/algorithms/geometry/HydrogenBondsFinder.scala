f69939c901187d15c0fce40db6f1dfd46cca3baf
Tatiana Malygina <merlettaia@gmail.com>
2015-10-28 12:24:59 +0300

    added hydrogen bonds - slow and without some constrains

--- scala_code/src/main/scala/ru/biocad/ig/common/algorithms/geometry/HydrogenBondsFinder.scala
+++ scala_code/src/main/scala/ru/biocad/ig/common/algorithms/geometry/HydrogenBondsFinder.scala
@@ -7,15 +7,27 @@ import ru.biocad.ig.common.structures.aminoacid.SimplifiedAminoacid
 case class HydrogenBondsFinder(HBondCondition : (Seq[SimplifiedAminoacid], Int, Int) => Boolean,
     structure : Seq[SimplifiedAminoacid]) {
   def findBondsForStructure(structure : Seq[SimplifiedAminoacid]) = {
-    (1 to structure.size - 1).map({ i =>
-      (i + 3 to structure.size - 1).filter({j =>
+    val hBonds = (0 to structure.size - 1).map({ i =>
+      (0 to structure.size - 1).map({j =>
         HBondCondition(structure, i, j)
       })
-    }).flatten
+    })
+    val b = (0 to structure.size - 1).map({i =>
+      (i + 1 to structure.size - 1).count({j=> hBonds(i)(j) })
+    }).foldLeft(0)(_ + _)
+    val c = (0 to structure.size - 1).map({i =>
+      (i + 1 to structure.size - 1).count({j=>
+
+          (j <= structure.size - 1 && i > 0 && hBonds(i)(j)) && (
+            (hBonds(i + 1)(j + 1)) ||
+            (hBonds(i - 1)(j + 1)))
+        })
+    }).foldLeft(0)(_ + _)
+
     //TODO: ...this is unfinished method, should return actual hydrogen bond network
+    (b, c)
   }
 
-  val cooperativeCount = 0 //TODO:implement
-  val bondsCount = 0//TODO: implement
+  val (bondsCount, cooperativeCount) = findBondsForStructure(structure)//TODO: implement
 
 }
