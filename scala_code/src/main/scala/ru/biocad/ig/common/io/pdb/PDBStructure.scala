ca6acd350e3bc1835331b281a3dad1218e97e941
Tatiana Malygina <merlettaia@gmail.com>
2015-10-28 18:22:14 +0300

    added iteration instead of grouping - works badly

--- scala_code/src/main/scala/ru/biocad/ig/common/io/pdb/PDBStructure.scala
+++ scala_code/src/main/scala/ru/biocad/ig/common/io/pdb/PDBStructure.scala
@@ -19,8 +19,35 @@ class PDBStructure {
   //TODO: should add method to remove water
 }
 
+
+class ChainIterator(input_sequence : Seq[PDBAtomInfo], chain : Char = 'L') extends Iterator[Seq[PDBAtomInfo]] {
+  var tailSequence : Seq[PDBAtomInfo] = skipToChainStart(input_sequence, chain)
+
+  def skipToChainStart(sequence : Seq[PDBAtomInfo], chain : Char) : Seq[PDBAtomInfo] = sequence match {
+    case Seq() => Seq()
+    case x if x.head.chainID == chain => x
+    case x if x.head.chainID != chain => {
+      skipToChainStart(x.tail, chain)
+    }
+  }
+
+  def hasNext : Boolean = !tailSequence.isEmpty && tailSequence.head.chainID == chain
+
+  def next() : Seq[PDBAtomInfo] = {
+    var prefix : Seq[PDBAtomInfo] = Seq()
+    val currentAA = tailSequence.head.resSeq
+    val currentChain = tailSequence.head.chainID
+    while (!tailSequence.isEmpty && tailSequence.head.chainID == currentChain && tailSequence.head.resSeq == currentAA) {
+        prefix = prefix :+ tailSequence.head
+        tailSequence = tailSequence.tail
+    }
+    prefix
+  }
+}
+
 //TODO: should extend all logic of pdb file reading, the following class is temporary solution for collecting atoms to aminoacids
 class PDBAminoAcidCollection(val basic_structure : PDBStructure) {
+  val aminoacidsByChain = new ChainIterator(basic_structure.parse_array.toSeq)
   val aminoacids = basic_structure.parse_array.groupBy(_.chainID).map(x=>(x._1, x._2.groupBy(_.resSeq))).toMap
   val aminoacidIds = basic_structure.parse_array.groupBy(_.chainID).map(x=>(x._1, x._2.map(_.resSeq).distinct)).toMap
 
