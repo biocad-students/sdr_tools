c05b433899318ad429720dac5891802b98e07c2c
Tatiana Malygina <merlettaia@gmail.com>
2015-10-28 11:07:16 +0300

    fixed bug with int formatting

--- scala_code/src/test/scala/PDBAtomInfoTests.scala
+++ scala_code/src/test/scala/PDBAtomInfoTests.scala
@@ -7,7 +7,7 @@ import ru.biocad.ig.common.io.pdb.PDBAtomInfo
 
 class PDBAtomInfoTests extends FlatSpec with Matchers {
   it should "save and restore PDBAtomInfo from/to String correctly" in {
-    val exampleInfo = PDBAtomInfo(1, "CA", '+',"ARG", 'L', 2, 'f', 0, 0, 0, 0,0, "ABDE", "C", "TT")
+    val exampleInfo = PDBAtomInfo(89, "CA", '+',"ARG", 'L', 29, 'f', 0, 0, 0, 0,0, "ABDE", "C", "TT")
     //added mess values to rarely used fields to ensure they are (de)serialized properly
     (exampleInfo.toString.length) should equal (80)
 
