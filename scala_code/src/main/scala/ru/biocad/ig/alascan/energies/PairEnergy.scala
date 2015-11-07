5746084f8f6cd1208ab5266d2306284ee810b0f4
Tatiana Malygina <merlettaia@gmail.com>
2015-11-07 03:20:28 +0300

    refactoring 2

---
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/energies/PairEnergy.scala
@@ -0,0 +1,43 @@
+package ru.biocad.ig.alascan.energies
+
+import ru.biocad.ig.common.structures.aminoacid.{SimplifiedChain, SimplifiedAminoacid}
+import ru.biocad.ig.alascan.constants.energy_terms._
+import scala.io.Source
+import spray.json._
+
+import EPairJsonProtocol._
+
+
+class PairEnergy(val rotamerRadiusInfo : RotamerRadiusInfo) extends BasicEnergy {
+
+  val epair : EPair = JsonParser(Source.fromURL(getClass.getResource("/MCDP_json/PMFHIX_SCALE.json")).getLines().mkString("")).convertTo[EPair]
+
+  //TODO: rewrite later
+  def get_E_two(i : Int, j : Int, ai : SimplifiedAminoacid, aj : SimplifiedAminoacid, f : Double) : Double = {
+    val rRepulsive = rotamerRadiusInfo.getRrep(ai.name, aj.name)
+    val rInteraction = rotamerRadiusInfo.getR(ai.name, aj.name)
+    val pairEnergy = epair.get(ai.name, aj.name)
+    (ai.rotamer - aj.rotamer).length match {
+      case x if x < rRepulsive => rotamerRadiusInfo.eRepulsive
+      case x if x < rInteraction && pairEnergy >= 0.0 && (j - i == 5 || j - i == 6) => pairEnergy * 0.6
+      case x if x < rInteraction && pairEnergy >= 0.0 => pairEnergy
+      case _ if j - i == 5 || j - i == 6 => pairEnergy * f * 0.6
+      case _ => pairEnergy * f
+    }
+  }
+
+  override def get(aminoacids : SimplifiedChain) : Double = {
+    val constAngle20 = math.pow(math.cos(math.toRadians(20.0)), 2)
+    (2 to aminoacids.size - 3).flatMap({
+      i => (i + 4 to aminoacids.size - 3).map({
+        j => {
+          //TODO: check actual +- 2 for f
+          val ui_uj = (aminoacids(i + 2).ca - aminoacids(i - 2).ca).normalize *
+                      (aminoacids(j + 2).ca - aminoacids(j - 2).ca).normalize
+          val f = 1.0 - math.pow(ui_uj * ui_uj - constAngle20, 2)
+          get_E_two(i, j, aminoacids(i), aminoacids(j), f)
+        }
+      })
+    }).sum
+  }
+}
---
+++ scala_code/src/main/scala/ru/biocad/ig/alascan/energies/TemplateEnergy.scala
@@ -0,0 +1,33 @@
+package ru.biocad.ig.alascan.energies
+
+import ru.biocad.ig.common.structures.aminoacid.SimplifiedChain
+import ru.biocad.ig.alascan.constants.energy_terms._
+import scala.io.Source
+import spray.json._
+
+import EPairJsonProtocol._
+
+class TemplateEnergy(val buildContactMap : (SimplifiedChain) => Array[Array[Boolean]]) extends BasicEnergy {
+  val epair : EPair = JsonParser(Source.fromURL(getClass.getResource("/MCDP_json/PMFHIX_SCALE.json")).getLines().mkString("")).convertTo[EPair]
+
+  override def get(chain : SimplifiedChain) : Double = {
+    val contactMap = buildContactMap(chain)
+    //TODO: check borders
+    (4 to chain.size - 5).flatMap({
+      i => (i + 4 to chain.size - 5).map({
+        j => {
+          Seq(-4,-3,3,4).map({k => {
+            (if (contactMap(i)(j) && contactMap(i + k)(j + k))
+              epair.apab(chain(i).name)(chain(j).name) +
+              epair.apab(chain(i + k).name)(chain(j + k).name)
+            else 0.0)+
+            (if (contactMap(i)(j) && contactMap(i + k)(j - k))
+              epair.apab(chain(i).name)(chain(j).name) +
+              epair.apab(chain(i + k).name)(chain(j - k).name)
+            else 0.0)}}).sum
+        }
+      })
+    }).sum
+    //???
+  }
+}
